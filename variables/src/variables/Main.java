cfdbd8397b91bdd515bf93e83220bbceca6b81b0
cuhadar8 <ibrahimcuhadar8@gmail.com>
2022-10-05 19:38:57 +0300

    Variables

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int ogrenciSayisi = 10;
+		String mesaj = "Öğrenci Sayısı: ";
+		
+		System.out.println("Öğrenci sayısı: 10");
+		System.out.println("Öğrenci sayısı: " + ogrenciSayisi);
+		System.out.println(mesaj + "10");
+		System.out.println(mesaj + ogrenciSayisi);
+	}
+
+}
